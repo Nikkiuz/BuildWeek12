2cb9a73ee3ea322ced40be81b7da8f25b3e72fc3
tommaso.panciroli <tommypanci@gmail.com>
2024-12-16 17:02:35 +0100

    updated activities

--- src/components/ActivitiesComponent.jsx
+++ src/components/ActivitiesComponent.jsx
@@ -1,15 +1,44 @@
 // const apiKey =
 //   'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NzVmZWUyNDBlYTI4NjAwMTUyOGI5M2UiLCJpYXQiOjE3MzQzNDAxMzMsImV4cCI6MTczNTU0OTczM30.md8JPlmC0A2aU2EjfOWOWkJl_7-KZoYs1j2LwK-s3PA '
 
+import { CardBody, CardTitle, Row } from 'react-bootstrap'
 import Button from 'react-bootstrap/Button'
 import Card from 'react-bootstrap/Card'
 
 const ActivitiesComponent = () => {
   const activities = [
     {
-      
-    }
+      intro: 'Tommaso ha pubblicato questo post - 3 giorni',
+      text: 'Sto studiando presso epicode',
+      cover: 'https://www.placecats.com/100/100',
+    },
+    {
+      intro: 'Tommaso ha pubblicato questo post - 3 giorni',
+      text: 'Sto studiando presso epicode',
+      cover: 'https://www.placecats.com/200/200',
+    },
+    {
+      intro: 'Tommaso ha pubblicato questo post - 3 giorni',
+      text: 'Sto studiando presso epicode',
+      cover: 'https://www.placecats.com/200/200',
+    },
+    {
+      intro: 'Tommaso ha pubblicato questo post - 3 giorni',
+      text: 'Sto studiando presso epicode',
+      cover: 'https://www.placecats.com/200/200',
+    },
+    {
+      intro: 'Tommaso ha pubblicato questo post - 3 giorni',
+      text: 'Sto studiando presso epicode',
+      cover: 'https://www.placecats.com/200/200',
+    },
+    {
+      intro: 'Tommaso ha pubblicato questo post - 3 giorni',
+      text: 'Sto studiando presso epicode',
+      cover: 'https://www.placecats.com/200/200',
+    },
   ]
+
   return (
     <Card className=" d-flex " style={{ backgroundColor: 'white' }}>
       <Card.Header className="d-flex align-items-center">
@@ -29,12 +58,21 @@ const ActivitiesComponent = () => {
         </div>
       </Card.Header>
       <Card.Body>
-        <Card.Title>Special title treatment</Card.Title>
-        <Card.Text>
-          With supporting text below as a natural lead-in to additional content.
-        </Card.Text>
+        <Row xs={1} sm={2} md={2} lg={1} className="g-1">
+          {activities.map((activity, index) => (
+            <Card key={index}>
+              <CardBody>
+                <CardTitle>{activity.intro}</CardTitle>
+                <div className="d-flex">
+                  <Card.Img src={activity.cover} style={{ width: '100px' }} />
+                  <Card.Text className="mx-3">{activity.text}</Card.Text>
+                </div>
+              </CardBody>
+            </Card>
+          ))}
+        </Row>
       </Card.Body>
-      <Card.Footer className="bg-white">2 days ago</Card.Footer>
+      <Card.Footer>Mostra di più</Card.Footer>
     </Card>
   )
 }
